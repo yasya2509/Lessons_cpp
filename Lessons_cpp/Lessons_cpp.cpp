ffa264a1014e186200b7d5d67539422a4d9dd740
M.Silin <M.Silin@VAIO>
2022-11-21 14:37:41 +0300

    Добавьте файлы проекта.

---
+++ Lessons_cpp/Lessons_cpp.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream> 
+using namespace std;
+void FillArr(int arr[], int size) //запонить масив числами от 1000 до 1010 
+{
+	for (int i = 0; i < size; ++i)
+	{
+		arr[i] = 1000 + i;
+	}
+}
+void MultArr(int arr[], int size) //увеличели каждый элемент масива в 10 раз 
+{
+	for (int i = 0; i < size; ++i)
+	{
+		arr[i] *= 10;
+	}
+}
+
+void PrintArr(int arr[], int size) //вывели значения масива в консоль 
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
+int main()
+{
+	const int size = 11;
+	int arr[size];
+	FillArr(arr, size);
+	MultArr(arr, size);
+	PrintArr(arr, size);
+
+
+	return 0;
+}
