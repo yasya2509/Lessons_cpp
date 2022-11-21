b3d18700575c00a357e706561e41879298f207b4
M.Silin <yaroslavasiiliina@gmail.com>
2022-11-21 16:00:56 +0300

    Заполнила двухмерный массив  нужными числами

--- Lessons_cpp/Lessons_cpp.cpp
+++ Lessons_cpp/Lessons_cpp.cpp
@@ -4,14 +4,12 @@ using namespace std;
 int main()
 {
     const int size_1 = 2;
-    const int size_2 = 3;
-    int arr[size_1][size_2] = {
-        {5,6,7},
-        {8,9,0}
-    };
+    const int size_2 = 7;
+    int arr[size_1][size_2];
+
     for (int i = 0; i < size_1; ++i) {
         for (int j = 0; j < size_2; ++j) {
-            //arr[i][j] = 
+            arr[i][j] = j + i * size_2;
             cout << arr[i][j] << ' ';
         }
         cout << endl;
