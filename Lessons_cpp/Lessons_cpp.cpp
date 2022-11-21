8022e2ffec948d635d25a4596055be7ffd059bea
M.Silin <yaroslavasiiliina@gmail.com>
2022-11-21 16:49:40 +0300

    сложила 2 двухмерных массива

--- Lessons_cpp/Lessons_cpp.cpp
+++ Lessons_cpp/Lessons_cpp.cpp
@@ -4,13 +4,26 @@ using namespace std;
 int main()
 {
     const int size_1 = 2;
-    const int size_2 = 7;
-    int arr[size_1][size_2];
+    const int size_2 = 4;
+ 
+    int arr_first[size_1][size_2] = {
+        {1,2,3,5},
+        {2,9,9,9}
+    };
+    int arr_second[size_1][size_2] = {
+        {9,8,7,5},
+        {8,1,1,1}
+    };
+
+   
 
     for (int i = 0; i < size_1; ++i) {
         for (int j = 0; j < size_2; ++j) {
-            arr[i][j] = j + i * size_2;
-            cout << arr[i][j] << ' ';
+
+            arr_first[i][j] += arr_second[i][j];
+            cout << arr_first[i][j] << ' ';
+
+           // cout << arr[i][j] << ' ';
         }
         cout << endl;
     } 
